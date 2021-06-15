566319cbd582891ac746cd0f7b8668e0a334c48a
swiftcoder2020 <jpswift84@gmail.com>
2021-06-15 19:50:36 +0000

    Add images of bsl alphabets

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+// create a function for each specific speed i.e. easy/medium/hard (slow/normal/fast)
+
+
+// declare an array variable
+const bslwords = ["bond", "social", "touch", "rnid", ];
+
+
+
+
+
+
+
+
+
+
+// extracted from Traversy Media "JavaScript Crash Course for Beginners"
+// declare a variable to create a score function
+
+let bslscore;
+bslscore = 10;
+console.log(bslscore);
+
+// my idea based on the above
+// const newbslgame = 0;
+// const correctanswer = 10;
+// const bslscore;
