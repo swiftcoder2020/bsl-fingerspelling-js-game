d61d03d2f901a7491e2810895c20d5ce4277cb06
swiftcoder2020 <jpswift84@gmail.com>
2021-06-19 14:12:21 +0000

    Add array of words & create random effect in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,15 @@
 // create a function for each specific speed i.e. easy/medium/hard (slow/normal/fast)
 
 
-// declare an array variable
-const bslwords = ["bond", "social", "touch", "rnid", ];
+// declare an array variable associated with deaf culture
+const bslwords = ["bond", "social", "touch", "rnid", "company", "dog", "attentive", 
+"express", "deaf", "sign", "hand", "mouth", "lip", "read", "visual", "contact", "eye",
+"fingers", "sense", "belong", "culture", "diverse", "values", "beliefs", "identity"];
+console.log(bslwords);
 
+// grab a random word from the above array variable named bslwords and put into a new array
+// source to select a random value:- https://stackoverflow.com/questions/4550505/getting-a-random-value-from-a-javascript-array
+const randombslword = Math.floor(Math.random() * bslwords.length);
+console.log(randombslword, bslwords[randombslword]);
 
-
-
-
-
-
-
-
-
-// extracted from Traversy Media "JavaScript Crash Course for Beginners"
-// declare a variable to create a score function
-
-let bslscore;
-bslscore = 10;
-console.log(bslscore);
-
-// my idea based on the above
-// const newbslgame = 0;
-// const correctanswer = 10;
-// const bslscore;
+// create another new array to split the word into letters
