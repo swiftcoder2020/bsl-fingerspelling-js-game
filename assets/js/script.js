59b4afa93b857906665495e5d296021dc84c9341
swiftcoder2020 <jpswift84@gmail.com>
2021-06-19 14:42:37 +0000

    Add improvised js code & source from w3schools in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,31 @@
+// extracted & improvised from https://www.w3schools.com/howto/howto_css_modals.asp
+// Game introductory modal
+// get the modal
+const gameModal = document.getElementById("gameIntroModal");
+
+// get the play button that opens the modal
+const playButton = document.getElementById("playButtonModal");
+
+// get the span element that closes the modal
+const span = document.getElementsByClassName("close")[0];
+
+// when the user clicks the button, open the modal
+playButton.onclick = function() {
+    gameModal.style.display = "block";
+}
+
+// when the user clicks on span element (x), close the modal
+span.onclick = function() {
+    gameModal.style.display = "none";
+}
+
+// when the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        gameModal.style.display = "none";
+    }
+}
+
 // create a function for each specific speed i.e. easy/medium/hard (slow/normal/fast)
 
 
