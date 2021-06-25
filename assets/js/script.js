06d84339cec6ca4f20d92ff8d287da0301881020
swiftcoder2020 <jpswift84@gmail.com>
2021-06-25 11:56:36 +0000

    Amend structure & comments in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,17 @@
 // @ts-check <--- refer to Gitpod Welcome, this is the JavaScript Type Checking to spot mistakes
 
-// lines 3 - 27 extracted & improvised from https://www.w3schools.com/howto/howto_css_modals.asp
 // Game introductory modal
-// get the modal and assign it to a variable
+// lines 8 - 39 extracted & improvised from https://www.w3schools.com/howto/howto_css_modals.asp
 // according to w3schools getElementById finds an HTML element with id e.g id="gameIntroModal"
-const gameModal = document.getElementById("gameIntroModal");
+
+// get the modal and assign it to a variable
+const gameModal = document.getElementById("gameIntroModal"); 
 
 // get the play button that opens the modal
-const playIconButton = document.getElementById("playButtonModal");
+const playIconButton = document.getElementById("playButtonModal"); 
 
 // get the span element that closes the modal
-const modalClose = document.getElementsByClassName("close")[0];
+const modalClose = document.getElementsByClassName("close")[0]; 
 
 // get each of the game speed levels defined within the buttons
 const gameEasy = document.getElementById("bslGameEasy");
@@ -37,16 +38,16 @@ window.onclick = function(event) {
     }
 }
 
-/* when the user/visitor clicks on the 'Play Game' button, the game simply commences by
-displaying a series of images */
+/* when the user/visitor clicks on the 'Play Game' button, the game simply 
+commences by displaying a series of images */
 commenceGame.onclick = function() {
     
 }
 
-/* create a function that determines which of each specific speed i.e. easy/medium/hard 
-(slow/normal/fast) has been clicked on so that a change in background colour can be applied, 
-yet only one of the three options can be selected. Then specify a speed as a time in 
-milliseconds. */
+/* create a function that determines which of each specific speed i.e. 
+easy/medium/hard (slow/normal/fast) has been clicked on so that a change in 
+background colour can be applied, yet only one of the three options can be 
+selected. Then specify a speed as a time in milliseconds. */
 function setLevel(level) {
     if (level === 1) {
         speedLevel = 3000;
