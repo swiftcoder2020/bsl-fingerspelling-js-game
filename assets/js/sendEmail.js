e38d187512b0a68c3a4c686c53399d72b7962c5b
swiftcoder2020 <jpswift84@gmail.com>
2021-07-04 14:38:26 +0000

    Amend to comment out alert function

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -7,12 +7,12 @@ function sendMail(contactForm) {
     .then(
         function(response) {
             console.log("SUCCESS", response);
-            alert("Thanks for submitting your response! I will be in touch with you soon!");
-			contactForm.reset();
+            // alert("Thanks for submitting your response! I will be in touch with you soon!");
+			// contactForm.reset();
         },
         function(error) {
             console.log("FAILED", error);
-            alert("Sorry, you were unsuccessful in sending an email, please try again later.");
+            // alert("Sorry, you were unsuccessful in sending an email, please try again later.");
         }
     );
     return false; // To block from loading a new page
