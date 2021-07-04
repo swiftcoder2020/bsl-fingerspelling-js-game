6630a00b6c591faff04a2910c4c75055b6f95dfa
swiftcoder2020 <jpswift84@gmail.com>
2021-07-04 14:30:54 +0000

    Amend function parameter name in sendEmail.js

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,18 +1,18 @@
-function sendMail(contactForm) {
+function sendMail(bslContactForm) {
     emailjs.send("Gmail_JS", "JS_EmailJS", {
-        "from_name": contactForm.fullname.value,
-        "from_email": contactForm.emailaddress.value,
-        "message": contactForm.message.value
+        "from_name": bslContactForm.fullname.value,
+        "from_email": bslContactForm.emailaddress.value,
+        "message": bslContactForm.message.value
     })
     .then(
         function(response) {
             console.log("SUCCESS", response);
-            alert("Thanks for submitting your response! I will be in touch with you soon!")
-			contactForm.reset();
+            alert("Thanks for submitting your response! I will be in touch with you soon!");
+			bslContactForm.reset();
         },
         function(error) {
             console.log("FAILED", error);
-            alert("Sorry, you were unsuccessful in sending an email, please try again later.")
+            alert("Sorry, you were unsuccessful in sending an email, please try again later.");
         }
     );
     return false; // To block from loading a new page
