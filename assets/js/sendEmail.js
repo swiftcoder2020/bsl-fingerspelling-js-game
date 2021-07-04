e0019536e881d1aef4858b8b893a5c20c31fbdac
swiftcoder2020 <jpswift84@gmail.com>
2021-07-04 14:52:22 +0000

    Amend to change value name

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,18 +1,18 @@
 function sendMail(contactForm) {
     emailjs.send("Gmail_JS", "JS_EmailJS", {
-        "from_name": contactForm.fullname.value,
-        "from_email": contactForm.emailaddress.value,
-        "message": contactForm.message.value
+        "from_name": contactForm.bslfullname.value,
+        "from_email": contactForm.bslemailaddress.value,
+        "message": contactForm.bslmessage.value
     })
     .then(
         function(response) {
             console.log("SUCCESS", response);
-            // alert("Thanks for submitting your response! I will be in touch with you soon!");
+            alert("Thanks for submitting your response! I will be in touch with you soon!");
 			contactForm.reset();
         },
         function(error) {
             console.log("FAILED", error);
-            // alert("Sorry, you were unsuccessful in sending an email, please try again later.");
+            alert("Sorry, you were unsuccessful in sending an email, please try again later.");
         }
     );
     return false; // To block from loading a new page
