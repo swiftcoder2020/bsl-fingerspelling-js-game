5d1bbc0ae2f8b44e2f721270118910736463b050
swiftcoder2020 <jpswift84@gmail.com>
2021-07-04 14:37:00 +0000

    Amend to correct sendEmail.js

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,14 +1,14 @@
-function sendMail(bslContactForm) {
+function sendMail(contactForm) {
     emailjs.send("Gmail_JS", "JS_EmailJS", {
-        "from_name": bslContactForm.fullname.value,
-        "from_email": bslContactForm.emailaddress.value,
-        "message": bslContactForm.message.value
+        "from_name": contactForm.fullname.value,
+        "from_email": contactForm.emailaddress.value,
+        "message": contactForm.message.value
     })
     .then(
         function(response) {
             console.log("SUCCESS", response);
             alert("Thanks for submitting your response! I will be in touch with you soon!");
-			bslContactForm.reset();
+			contactForm.reset();
         },
         function(error) {
             console.log("FAILED", error);
